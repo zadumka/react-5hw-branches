6de6011af4d6407189d9563942bc46cc8dd844a5
zadumka <90329284+zadumka@users.noreply.github.com>
2024-02-16 18:21:09 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Homepage from "./pages/Homepage";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrawserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
