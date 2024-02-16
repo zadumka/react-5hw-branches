8530fb0fbde2642d920fd6aaf38dd4e75c350c68
zadumka <90329284+zadumka@users.noreply.github.com>
2024-02-16 17:45:52 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
+import Homepage from "./pages/Homepage";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Browserhhjh>
+    <Brawserhhjh>
       < Homepage/>
-    </Browserhhjh>
+    </Brawserhhjh>
   </React.StrictMode>
 );
